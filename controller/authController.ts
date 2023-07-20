7906adf9af13bec89f8a2bdde9eb53017bbe9f72
Martin Doherty <martin.doherty@kainos.com>
2023-07-20 14:25:05 +0100

    login changes

---
+++ controller/authController.ts
@@ -0,0 +1,28 @@
+import {Application, Response, Request} from "express";
+import {Login} from "../model/auth";
+
+const authService = require('../service/authSerivce')
+
+module.exports = function (app: Application)
+{
+    app.get('/login', async(req: Request,res:Response)=>
+    {
+        res.render('login')
+    })
+
+    app.post('/login', async (req:Request,res:Response) =>
+    {
+        let data: Login = req.body;
+        try{
+            req.session.token = await authService.login(data)
+
+            res.redirect('/job-roles')
+        } catch (e)
+        {
+            console.error(e)
+            res.locals.errormessage = e.message
+            res.render('login',req.body)
+        }
+    })
+
+}
---
+++ validator/loginValidator.ts
@@ -0,0 +1,7 @@
+import {Login} from "../model/auth";
+module.exports.validateLogin = function (login: Login): String
+{
+    //if(login.username.match())
+
+    return null;
+}
