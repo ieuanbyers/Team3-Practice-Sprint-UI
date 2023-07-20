297de45b66dba21610fa0a56b6a1c174294a0a7e
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-07-20 09:51:42 +0100

    Removed incomplete tests

--- Tests/Unit/RoleBandServiceTests.ts
+++ Tests/Unit/RoleBandServiceTests.ts
@@ -18,28 +18,3 @@ const employee = {
     bankNo: "12345678",
     nin: "nin"
 }
-
-describe('EmployeeService', function () {
-    describe('getEmployees', function () {
-      it('should return employees from response', async () => {
-        var mock = new MockAdapter(axios);
-
-        const data = [employee];
-
-        mock.onGet(EmployeeService.URL).reply(200, data);
-
-        var results = await EmployeeService.getEmployees();
-
-        expect(results[0]).to.deep.equal(employee)
-      })
-
-      it('should throw exception when 500 error returned from axios', async () => {
-        var mock = new MockAdapter(axios);
-
-        mock.onGet(EmployeeService.URL).reply(500);
-
-        var error = await EmployeeService.getEmployees()
-        
-        expect(error.message).to.equal('Could not get employees')
-      })
-    })
