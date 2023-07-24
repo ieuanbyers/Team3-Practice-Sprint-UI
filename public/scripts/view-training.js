d58d6b70a235c3e62e91bc5f7a8e781f381951a9
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-24 15:30:03 +0100

    Fixed formatting in script file

--- public/scripts/view-training.js
+++ public/scripts/view-training.js
@@ -1,10 +1,14 @@
-let options = {searchable: true, sortable: true, classes: {
-    active: "active",
-    disabled: "disabled",
-    selector: "form-select",
-    paginationList: "pagination",
-    paginationListItem: "page-item",
-    paginationListItemLink: "page-link",
-    input: "form-control"
-}};
+let options = {
+        searchable: true, 
+        sortable: true, 
+        classes: {
+            active: "active",
+            disabled: "disabled",
+            selector: "form-select",
+            paginationList: "pagination",
+            paginationListItem: "page-item",
+            paginationListItemLink: "page-link",
+            input: "form-control"
+        }
+    };
 const dataTable = new simpleDatatables.DataTable("#training-table", options)
