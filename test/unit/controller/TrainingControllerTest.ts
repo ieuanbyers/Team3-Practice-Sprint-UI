c2e54f848ea7965ac0af26c17e633c70afcc57c0
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-25 11:59:01 +0100

    Added unit test for training controller and updated test command to include --exit

--- test/unit/controller/TrainingControllerTest.ts
+++ test/unit/controller/TrainingControllerTest.ts
@@ -1,1 +1,20 @@
+const supertest = require('supertest');
+import {app} from "../../../app";
+const sinon = require('sinon');
 
+const trainingService = require('../../../service/trainingService');
+
+describe('TrainingController', function () {
+    it('should return a 200 status code', async ()=>
+    {
+        const stub = sinon.stub(trainingService,"getTrainingByBand").returns({
+            name: 'Test Course',
+            link: 'http://localhost:3000',
+            category: 'Test Category',
+        });
+        const bandId: number = 2;
+
+        const response = await supertest(app)
+            .get(`/view-training/${bandId}`).set('Accept','application/json').expect(200);
+    })
+})
