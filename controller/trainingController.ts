b370b754a37a99cf16b5a72ba649d2eec8eb65ce
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-25 09:06:42 +0100

    Removed unused call to get training categories

--- controller/trainingController.ts
+++ controller/trainingController.ts
@@ -6,10 +6,8 @@ const trainingService = require('../service/TrainingService')
 module.exports = function (app: Application) {
   app.get('/view-training/:id', async (req: Request, res: Response) => {
     let data: Training[];
-    let categories: string[];
 
     try {
-      categories = await trainingService.getTrainingCategories();
       data = await trainingService.getTrainingByBand(req.params.id);
     } catch (e) {
       console.error(e)
