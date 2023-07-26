fb38a38a4aa7a262a8f3fa8901eff686995778a0
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-26 11:18:58 +0100

    Fixed type in app.ts

--- app.ts
+++ app.ts
@@ -26,7 +26,7 @@ app.use(express.json())
 
 app.use(express.urlencoded({extended: true}));
 
-app.use(session({secret: 'NOT HARDODED SECRET', cookie: {maxAge: 60000}}));
+app.use(session({secret: 'NOT HARDCODED SECRET', cookie: {maxAge: 60000}}));
 declare module "express-session"
 {
     interface SessionData{
