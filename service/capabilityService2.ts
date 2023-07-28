3a60f28463d38b60ab6bc5d517921b541c6c7b1f
stuferg <stuart.ferguson@kainos.com>
2023-07-28 16:06:54 +0100

    changing file names

--- service/capabilityService2.ts
+++ service/capabilityService2.ts
@@ -2,8 +2,6 @@ import axios, { AxiosResponse } from "axios";
 import { Capability } from "../model/Capability";
 import { FailedToGetCapabilitiesError } from "../Errors/FailedToGetCapabilitiesError";
 
-
-
 module.exports.getCapabilities = async function(): Promise<Capability[]>
 {
     try
@@ -17,6 +15,3 @@ module.exports.getCapabilities = async function(): Promise<Capability[]>
         throw new FailedToGetCapabilitiesError('Could not get capabilities')
     }
 }
-
-
-
