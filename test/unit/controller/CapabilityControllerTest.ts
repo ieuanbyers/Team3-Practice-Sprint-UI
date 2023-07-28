692ea4e3d0887e38b52fb2a05911ffb758fafbe5
stuferg <stuart.ferguson@kainos.com>
2023-07-28 17:03:10 +0100

    Fixing controller test

--- test/unit/controller/CapabilityControllerTest.ts
+++ test/unit/controller/CapabilityControllerTest.ts
@@ -26,3 +26,24 @@ describe('CapabilityController', function () {
         serviceStub.restore();
     })
 })
+
+describe('competecnyController', function () {
+    let serviceStub: SinonStub;
+
+    before(() => {
+        serviceStub = sinon.stub(capabilityService, "createCapability").returns({
+            name: "Test",
+            description: "Testing",
+        });
+    })
+
+    it('should return http code 200 when successfully creates capability', async ()=>
+    {
+        await supertest(app)
+            .get('/add-capability').set('Accept','application/json').expect(200);
+    })
+
+    after(() => {
+        serviceStub.restore();
+    })
+})
