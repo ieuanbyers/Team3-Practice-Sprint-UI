ec86b39a07646adac08958148b38d55573d4c442
stuferg <stuart.ferguson@kainos.com>
2023-07-28 16:24:25 +0100

    Fixing controller

--- controller/capabilityController.ts
+++ controller/capabilityController.ts
@@ -5,6 +5,31 @@ const capabilityService = require('../service/CapabilityService')
 
 module.exports = function(app: Application)
 {
+
+    app.get('/success', async (req: Request, res: Response) => {
+        res.render('success')
+    })
+
+    app.get('/add-capability', async (req: Request, res: Response) => {
+        res.render('add-capability')
+    })
+
+    app.post('/add-capability', async (req: Request, res: Response) => {
+        let data: CapabilityRequest = req.body
+        let id: Number
+
+        try {
+            id = await CapabilityService.createCapability(data)
+
+            res.redirect('success')
+        } catch (e) {
+            console.error(e);
+
+            res.locals.errormessage = e.message
+
+            res.render('add-capabilty', req.body)
+        }
+    }) 
     app.get('/capability', async(req: Request, res: Response) =>
     {
         let data: Capability[];
