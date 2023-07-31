21c1d1206dd71b263dca588c8ed95c2e8223f835
Martin Doherty <martin.doherty@kainos.com>
2023-07-31 14:45:50 +0100

    review changhes

--- test/unit/controller/AuthControllerTest.ts
+++ test/unit/controller/AuthControllerTest.ts
@@ -1,13 +1,13 @@
 import supertest from "supertest";
 import {app} from "../../../app";
-import sinon from "sinon";
+import sinon, {SinonStub} from "sinon";
 
 const authService = require('../../../service/authService');
 
 describe('authController', function () {
+    let stub: SinonStub;
     it('check for 200 code on get', async ()=>
     {
-
         const response= await supertest(app)
             .get('/login').set('Accept','application/json').expect(200);
         sinon.restore()
@@ -15,17 +15,18 @@ describe('authController', function () {
 
     it('check for 302 code on post with valid login', async ()=>
     {
-        const stub = sinon.stub(authService,"login").returns("token");
+        stub = sinon.stub(authService,"login").returns("token");
         const response= await supertest(app)
             .post('/login').set('Accept','application/json').expect(302);
-        sinon.restore()
     })
 
     it('check for 200 code on post with invalid login', async ()=>
     {
-        const stub = sinon.stub(authService,"login").throws(Error("could not login"));
+        stub = sinon.stub(authService,"login").throws(Error("could not login"));
         const response= await supertest(app)
             .post('/login').set('Accept','application/json').expect(200);
-        sinon.restore()
+    })
+    afterEach (() =>{
+        sinon.restore();
     })
 })
