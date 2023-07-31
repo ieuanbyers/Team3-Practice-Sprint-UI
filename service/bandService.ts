ef3e1087e62804209b452b917db38ca21b01bc37
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-31 12:24:12 +0100

    Updated tests to set baseurl in before block

--- service/bandService.ts
+++ service/bandService.ts
@@ -1,14 +1,14 @@
 import { AxiosResponse } from 'axios';
 import { type Band } from '../model/band'
 import { FailedToGetBandError } from '../Errors/failedToGetBandError';
+import { getEnvConfig } from './configService';
 const axios = require('axios')
 
-axios.defaults.baseURL = process.env.API_URL;
 module.exports.URL = '/api/bands/';
 
 module.exports.getBandById = async function (bandId: number): Promise<Band> {
     try {
-        const endpointURL = axios.defaults.baseURL + module.exports.URL;
+        const endpointURL = getEnvConfig().api_url + module.exports.URL;
         const response: AxiosResponse = await axios.get(endpointURL + bandId);
     
         return response.data;
--- service/trainingService.ts
+++ service/trainingService.ts
@@ -1,9 +1,10 @@
 import { AxiosResponse } from 'axios';
 import { FailedToGetTrainingError } from '../Errors/failedToGetTrainingError';
 import { type Training } from '../model/training'
+import { getEnvConfig } from './configService';
 const axios = require('axios')
 
-axios.defaults.baseURL = process.env.API_URL;
+
 module.exports.URL = '/api/training/';
 
 module.exports.getTrainingByBand = async function (bandId: number): Promise<Training[]> {
@@ -11,7 +12,7 @@ module.exports.getTrainingByBand = async function (bandId: number): Promise<Trai
     throw new FailedToGetTrainingError('Band ID does not exist');
   }
   try {
-    const endpointURL = axios.defaults.baseURL + module.exports.URL;
+    const endpointURL = getEnvConfig().api_url + this.URL;
     const response: AxiosResponse = await axios.get(endpointURL + bandId);
 
     return response.data;
